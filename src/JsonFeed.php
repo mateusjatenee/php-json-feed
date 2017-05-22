324801c104f387b17f85267327553afaf7cdd8c6
Mateus Guimarães <mateus.jatene@gmail.com>
2017-05-22 01:47:05 -0300

    docblock

--- src/JsonFeed.php
+++ src/JsonFeed.php
@@ -8,10 +8,16 @@ use Mateusjatenee\JsonFeed\Exceptions\IncorrectFeedStructureException;
 
 class JsonFeed
 {
+    /**
+     * @var array
+     */
     protected $requiredProperties = [
         'title',
     ];
 
+    /**
+     * @var array
+     */
     protected $acceptedProperties = [
         'version', 'title', 'home_page_url',
         'feed_url', 'description', 'icon',
@@ -19,23 +25,48 @@ class JsonFeed
         'author', 'user_comment', 'hubs',
     ];
 
+    /**
+     * @var string
+     */
     protected $version = 'https://jsonfeed.org/version/1';
 
+    /**
+     * @var mixed
+     */
     protected $properties;
 
+    /**
+     * @var mixed
+     */
     protected $items;
 
+    /**
+     * @param array $properties
+     * @param array $items
+     */
     public function __construct(array $properties = [], array $items = [])
     {
         $this->properties = new Collection($properties);
         $this->items = new Collection($items);
     }
 
+    /**
+     * Returns an instance of the class
+     *
+     * @param array $properties
+     * @return self
+     */
     public static function start($properties = [])
     {
         return new static($properties);
     }
 
+    /**
+     * Builds a collection following the JSON Feed spec
+     *
+     * @throws \Mateusjatenee\JsonFeed\Exceptions\IncorrectFeedStructureException
+     * @return \Illuminate\Support\Collection
+     */
     public function build()
     {
         if (!$this->hasCorrectStructure()) {
@@ -54,21 +85,42 @@ class JsonFeed
         return $properties;
     }
 
+    /**
+     * Builds the collection and converts it to an array
+     *
+     * @return array
+     */
     public function toArray()
     {
         return $this->build()->toArray();
     }
 
+    /**
+     * Returns an array of accepted properties
+     *
+     * @return array
+     */
     public function getAcceptedProperties()
     {
         return $this->acceptedProperties;
     }
 
+    /**
+     * Checks if the properties includes the required ones
+     *
+     * @return boolean
+     */
     protected function hasCorrectStructure()
     {
         return $this->filterProperties($this->requiredProperties)->count() === count($this->requiredProperties);
     }
 
+    /**
+     * Filter properties collection to only include accepted properties
+     *
+     * @param $array
+     * @return mixed
+     */
     protected function filterProperties($array = null)
     {
         $array = $array ?? $this->acceptedProperties;
@@ -78,6 +130,12 @@ class JsonFeed
         });
     }
 
+    /**
+     * Dynamically gets a property
+     *
+     * @param $property
+     * @return string | null
+     */
     protected function getProperty($property)
     {
         if ($this->properties->has($property)) {
@@ -85,11 +143,23 @@ class JsonFeed
         }
     }
 
+    /**
+     * Gets the JSON Feed version being used.
+     *
+     * @return string
+     */
     public function getVersion()
     {
         return $this->version;
     }
 
+    /**
+     * Handle dynamic methods calls
+     *
+     * @param $method
+     * @param $parameters
+     * @return mixed
+     */
     public function __call($method, $parameters)
     {
         if (substr($method, 0, 3) == 'get') {
@@ -101,6 +171,9 @@ class JsonFeed
         throw new BadMethodCallException("Call to undefined method {$className}::{$method}()");
     }
 
+    /**
+     * @return mixed
+     */
     protected function buildItems()
     {
         return $this->items->map(function ($item) {
