bebda1fec4ade1d071f04cd78044dd6c2c170a25
Mateus Guimarães <mateus.jatene@gmail.com>
2017-05-22 03:07:37 -0300

    basic config file

---
+++ config/laravel-json-feed.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title' => '',
+    'home_page_url' => env('APP_URL'),
+    'should_not_appear' => '',
+    'feed_url' => '',
+    'author' => [
+        'url' => '',
+        'name' => '',
+    ],
+    'icon' => '',
+    'favicon' => '',
+];
--- src/JsonFeedServiceProvider.php
+++ src/JsonFeedServiceProvider.php
@@ -7,6 +7,13 @@ use Mateusjatenee\JsonFeed\JsonFeed;
 
 class JsonFeedServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/laravel-json-feed.php' => config_path('laravel-json-feed.php'),
+        ], 'config');
+    }
+
     public function register()
     {
         $this->app->bind('jsonFeed', function ($app) {
