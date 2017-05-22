8acc5b17b6c1046c4dcac68d3b186ccbd11cdb33
Mateus Guimarães <mateus.jatene@gmail.com>
2017-05-22 00:05:23 -0300

    only return allowed properties

--- src/JsonFeed.php
+++ src/JsonFeed.php
@@ -2,21 +2,47 @@
 
 namespace Mateusjatenee\JsonFeed;
 
+use Illuminate\Support\Collection;
+
 class JsonFeed
 {
     protected $requiredProperties = [
         'version', 'title',
     ];
 
+    protected $acceptedProperties = [
+        'version', 'title', 'home_page_url',
+        'feed_url', 'description', 'icon',
+        'next_url', 'expired', 'favicon',
+        'author', 'user_comment', 'hubs',
+    ];
+
     protected $properties;
 
     public function __construct(array $properties = [])
     {
-        $this->properties = $properties;
+        $this->properties = new Collection($properties);
     }
 
     public static function start($properties = [])
     {
         return new static($properties);
+    }
+
+    public function toArray()
+    {
+        return $this->filterProperties($this->properties)->all();
+    }
+
+    public function getAcceptedProperties()
+    {
+        return $this->acceptedProperties;
+    }
+
+    protected function filterProperties(Collection $properties)
+    {
+        return $properties->filter(function ($value, $property) {
+            return in_array($property, $this->acceptedProperties);
+        });
     }
 }
--- tests/JsonFeedTest.php
+++ tests/JsonFeedTest.php
@@ -10,4 +10,20 @@ class JsonFeedTest extends TestCase
     {
         $this->assertInstanceOf(JsonFeed::class, JsonFeed::start());
     }
+
+    /** @test */
+    public function it_only_returns_accepted_properties()
+    {
+        $properties = collect(app('jsonFeed')->getAcceptedProperties());
+        $properties = $properties->flatMap(function ($value, $key) {
+            return [
+                $value => 'foo',
+            ];
+        })->all();
+
+        $feed = JsonFeed::start(array_merge($properties, $arr = ['foo' => 'bar']));
+
+        $this->assertEquals($properties, $feed->toArray());
+        $this->assertNotContains($arr, $feed->toArray());
+    }
 }
